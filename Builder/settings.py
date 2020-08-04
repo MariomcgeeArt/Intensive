59dbfeed70e9dfbe05c9474441e322cacd6d0770
Mario McGee <mario.mcgee@students.makeschool.com>
2020-08-04 15:57:45 -0700

    tweaked settings for deployment

--- Builder/settings.py
+++ Builder/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'a9uv%j-f8%mx%=8#g54*9^najsa*ke&18_q!ck=0^6(*!$s595'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'dailyaffirmation.herokuapp.com']
 
 
 # Application definition
@@ -100,6 +100,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+
 
 
 # Internationalization
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Builder.wsgi --log-file -
